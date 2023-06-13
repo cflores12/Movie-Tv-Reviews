26778a74a998050f88d8478f7df5c4678e5e1606
flores <cflores12@users.noreply.github.com>
2023-06-12 20:52:24 -0400

    add background image

--- src/pages/films.tsx
+++ src/pages/films.tsx
@@ -1,3 +1,27 @@
+interface Movie {
+  id: string;
+  name: string;
+  image: string;
+  likes: number;
+  views: number;
+}
+
+function buildMovie({
+  id = "1",
+  name = "Super Mario Bros",
+  image = "Something",
+  likes = 40,
+  views = 60,
+}: Partial<Movie> = {}): Movie {
+  return {
+    id,
+    name,
+    image,
+    likes,
+    views,
+  };
+}
+
 const Films = () => {
   return <div>Start Films List</div>;
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,19 @@
 const Home = () => {
-  return <div className="starter">
-          <h2>Track films and Tv Shows you've watched.</h2>
-          <h2>Save and Share those you want to see.</h2>
-          <h2>Tell your Friends and Family what's good.</h2>
-        </div>;
+  return (
+    <div className="starter">
+      <div className="starter-top">
+        <h2>Track films and Tv Shows you've watched.</h2>
+        <h2>Save and Share those you want to see.</h2>
+        <h2>Tell your Friends and Family what's good.</h2>
+      </div>
+      <div>
+        Recent Films with hover effect of likes and views
+      </div>
+      <div>
+        Popular reviews this week wtih column for popular lists
+      </div>
+    </div>
+  );
 };
 
 export default Home;
