ac170f76709a54c9472c80074ef8b2c738d82e58
flores <cflores12@users.noreply.github.com>
2023-06-17 17:04:39 -0400

    finish hover effect for for moives with most views and likes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,7 @@
 import { useGetMoviesOnlyViewsLikes } from "../components/hooks/use-queries";
+import imgAvatar from "./img/img_avatar.png";
+import eye from "./img/eye.svg";
+import heart from "./img/heart.svg";
 
 const Movies = {
   movie1: {
@@ -7,6 +10,20 @@ const Movies = {
     image: "Somethuing Here",
     likes: 12,
     views: 12,
+    reviews: {
+      review1: {
+        id: 1,
+        comment: "Trash Moive dont see!",
+        userName: "dan",
+        stars: 1,
+      },
+      review2: {
+        id: 2,
+        comment: "Good Movie must watch!",
+        userName: "bob",
+        stars: 4,
+      },
+    },
   },
   movie2: {
     id: 2,
@@ -14,6 +31,20 @@ const Movies = {
     image: "Something Here",
     likes: 22,
     views: 22,
+    reviews: {
+      review1: {
+        id: 1,
+        comment: "Die of Laughter! Must Watch!",
+        userName: "dan",
+        stars: 5,
+      },
+      review2: {
+        id: 2,
+        comment: "Good movie could be better",
+        userName: "bob",
+        stars: 3,
+      },
+    },
   },
 };
 
@@ -26,21 +57,41 @@ const Home = () => {
         <h2>Tell your Friends and Family what's good.</h2>
       </div>
       <div className="most-view-like">
-        <p>Recent Films with hover effect of likes and views</p>
-        <div>
-          <span>
-            <p>{Movies.movie1.name}</p>
-            <p>{Movies.movie1.likes}</p>
-            <p>{Movies.movie1.views}</p>
-          </span>
-          <span>
-            <p>{Movies.movie2.name}</p>
-            <p>{Movies.movie2.likes}</p>
-            <p>{Movies.movie2.views}</p>
-          </span>
+        <span className="card">
+          <img src={imgAvatar} className="card-movie" />
+          <div className="card-middle-box">
+            <div className="box">
+              <img src={eye} />
+              <p>5,000</p>
+              <img src={heart} />
+              <p>10,000</p>
+            </div>
+          </div>
+        </span>
+        <span className="card">
+          <img src={imgAvatar} className="card-movie" />
+        </span>
+        <span className="card">
+          <img src={imgAvatar} className="card-movie" />
+        </span>
+        <span className="card">
+          <img src={imgAvatar} className="card-movie" />
+        </span>
+        <span className="card">
+          <img src={imgAvatar} className="card-movie" />
+        </span>
+        <span className="card">
+          <img src={imgAvatar} className="card-movie" />
+        </span>
+      </div>
+      <div className="playlists-comments">
+        <div className="recent-comments">
+          <p>This is for most recent reviews with comments</p>
+        </div>
+        <div className="popular-playlists">
+          <p>This is for the most popular playlits</p>
         </div>
       </div>
-      <div>Popular reviews this week wtih column for popular lists</div>
     </div>
   );
 };
